1625dc1e33de02da7d1235e5ab4466234c8bf625
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-12 22:01:58 -0400

    picking the syntax back up

--- hello-world.cpp
+++ hello-world.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 using std::cout;
 
-int main() {
-    cout << "Hello World!" << endl; // print Hello World
-    return 0;
+int main()
+{
+    int numberOfCats = 5;
+    int numberOfDogs = 7;
+    int totalNumberOfAnimals = numberOfCats + numberOfDogs;
+    cout << "Number of cats: " << numberOfCats << endl; // print Hello World
+    cout << "Number of dogs: " << numberOfDogs << endl;
+    totalNumberOfAnimals = totalNumberOfAnimals + 1;
+    cout << "Total number of animals: " << totalNumberOfAnimals << endl;
+
+    cout << "new dog arrived! " << endl;
+    // numberOfDogs++;
+    
+
+    cout << "New number of dogs: " << numberOfDogs << endl;
+    cout << "New number of animals: " << totalNumberOfAnimals << endl;
+    
+    
+        return 0;
 }
